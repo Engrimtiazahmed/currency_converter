1114f0646da4f5fb29fb25097900bd453aef3b58
Engrimtiazahmed <160152394+Engrimtiazahmed@users.noreply.github.com>
2024-04-06 12:53:12 +0500

    created currency converter using typescript and inquirer
    Created currency converter using typescript and inquirer

---
+++ index.ts
@@ -0,0 +1,44 @@
+#! /urs/bin/env node
+
+//Currency_Converter
+
+import inquirer from "inquirer";
+const currency: any = 
+{
+    PKR: 1,         //base rate
+    USD: 0.0036,
+    EUR: 0.0033,
+    CHY: 0.026,
+    INR: 0.30,
+    AFN: 0.26,
+  
+};
+
+let userAnswer = await inquirer.prompt([
+  {
+    name: "from",
+    message: "Enter from currency:",
+    type: "list",
+    choices: ["PKR","USD", "EUR", "CHY", "INR", "AFN"],
+  },
+  {
+    name: "to",
+    message: "Enter to currency:",
+    type: "list",
+    choices: ["PKR","USD", "EUR", "CHY", "INR", "AFN"],
+  },
+  {
+    name: "amount",
+    message: "Enter your amount:",
+    type: "number",
+  },
+]);
+
+
+let fromAmount = currency[userAnswer.from];
+let toAmount = currency[userAnswer.to];
+let amount = userAnswer.amount;
+let baseAmount = amount / fromAmount;
+let convertedAmount = baseAmount * toAmount;
+
+console.log(convertedAmount);
